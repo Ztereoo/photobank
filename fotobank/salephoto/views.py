e745255f0b2646be24fc825496769c363622071d
stereo <airstarview@gmail.com>
2024-10-15 17:22:43 +0300

    Pagination added

--- fotobank/salephoto/views.py
+++ fotobank/salephoto/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 from .models import Photo
@@ -9,7 +10,11 @@ def index(request):
 
 def allphoto(request):
     photo= Photo.objects.all()
-    return render(request, 'allphoto.html', {'photo': photo})
+    paginator = Paginator(photo, 6)  # Отображать по 6 фото на странице (можете изменить количество)
+
+    page_number = request.GET.get('page')  # Получить номер страницы из запроса
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'allphoto.html', {'photo': photo,'page_obj':page_obj})
 
 def photo_detail(request,pk):
     photo=get_object_or_404(Photo,pk=pk)
